19f002853edcd19a3b130e6b697266b998d9008b
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2020-05-30 20:54:45 +0530

    Changes made in the filtering logic

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter,SearchFilter
+from rest_framework import filters
 from drf_multiple_model.views import ObjectMultipleModelAPIView
 from rest_framework import generics
 
@@ -26,7 +26,12 @@ class BrowseViewSet(viewsets.ModelViewSet):
     """Searching and filtering bookmark feed items"""
     serializer_class = BookmarkSerializer
     queryset = Bookmark.objects.all()
-    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+
+    # filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filter_backends = (filters.SearchFilter,filters.OrderingFilter,)
+
     filter_fields = ('customer__customerid', 'customer__customerlan', 'customer__customerlon','title')
-    search_fields = ('customer__customerid', 'customer__customerlan', 'customer__customerlon','title')
+    
+    search_fields = ('customer__customerid', 'customer__customerlan', 'customer__customerlon','title',)
+
     OrderingFields = ('bid', 'title', 'url', 'sourcename','creationdate')
